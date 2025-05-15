4e4b35785fca1d70e920b6f82dafab917b5d790d
Seob0707 <rkddytjq000@gmail.com>
2025-05-15 15:12:11 +0900

    o/u/o 수정 ver1.2

--- pbl2/api/main.py
+++ pbl2/api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 import os
 
 from api.routers.user_router      import router as user_router
