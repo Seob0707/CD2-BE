0183b29e66308a963cd870b2387b9318050ae224
Seob0707 <rkddytjq000@gmail.com>
2025-05-19 19:16:21 +0900

    스웨그 로그인 수정

--- pbl2/api/routers/user_router.py
+++ pbl2/api/routers/user_router.py
@@ -15,13 +15,18 @@ async def signup(user: user_schema.UserCreate, db: AsyncSession = Depends(get_db
     return {"message": "회원가입 성공", "user_id": new_user.user_id}
 
 @router.post("/login", response_model=user_schema.Token)
+@router.post("/login", response_model=user_schema.Token)
 async def login(user: user_schema.UserLogin, db: AsyncSession = Depends(get_db)):
     db_user = await user_service.authenticate_user(db, user.email, user.password)
     if not db_user:
         raise HTTPException(status_code=401, detail="이메일 또는 비밀번호가 틀렸습니다.")
-    
-    access_token = security.create_access_token(data={"sub": str(db_user.user_id),"role": db_user.role})
-    return {"access_token": access_token}
+
+    access_token = security.create_access_token(data={"sub": str(db_user.user_id), "role": db_user.role})
+    return {
+        "access_token": access_token,
+        "token_type": "bearer", 
+        "user_id": db_user.user_id 
+    }
 
 @router.get("/me")
 async def read_my_profile(current_user: User = Depends(get_current_user)):
